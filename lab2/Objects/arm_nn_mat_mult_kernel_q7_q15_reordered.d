42de07d4ee5e88c1fe8103e89e915ab2dbfbaab1
Oleg Bondar <olegb5195@gmail.com>
2019-03-06 00:45:58 +0300

    Add lab 2

---
+++ lab2/Objects/arm_nn_mat_mult_kernel_q7_q15_reordered.d
@@ -0,0 +1,4 @@
+.\objects\arm_nn_mat_mult_kernel_q7_q15_reordered.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\NN\Source\ConvolutionFunctions\arm_nn_mat_mult_kernel_q7_q15_reordered.c
+.\objects\arm_nn_mat_mult_kernel_q7_q15_reordered.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\NN\Include\arm_nnfunctions.h
+.\objects\arm_nn_mat_mult_kernel_q7_q15_reordered.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\NN\Include\arm_nnsupportfunctions.h
+.\objects\arm_nn_mat_mult_kernel_q7_q15_reordered.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\DSP\Include\arm_math.h
---
+++ lab2/Objects/arm_nntables.d
@@ -0,0 +1,3 @@
+.\objects\arm_nntables.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\NN\Source\NNSupportFunctions\arm_nntables.c
+.\objects\arm_nntables.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\NN\Include\arm_nnsupportfunctions.h
+.\objects\arm_nntables.o: H:\Program\Keil\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\DSP\Include\arm_math.h
