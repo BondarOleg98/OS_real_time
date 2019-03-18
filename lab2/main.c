e6591245573b49841f637a337eab3ba3a53b988b
Oleg Bondar <olegb5195@gmail.com>
2019-03-18 14:12:24 +0300

    Update main.c

--- lab2/main.c
+++ lab2/main.c
@@ -7,17 +7,16 @@
 #define prescaler 720
 
 void initLED1(){
-		GPIO_InitTypeDef InitPort;
-		TIM_TimeBaseInitTypeDef timer;
-		TIM_OCInitTypeDef timerPWM;
+	GPIO_InitTypeDef InitPort;
+	TIM_TimeBaseInitTypeDef timer;
+	TIM_OCInitTypeDef timerPWM;
 	
-		GPIO_StructInit(&InitPort);
+	GPIO_StructInit(&InitPort);
   	InitPort.GPIO_Mode = GPIO_Mode_AF_PP;
   	InitPort.GPIO_Pin = GPIO_Pin_8;
   	InitPort.GPIO_Speed = GPIO_Speed_2MHz;
   	GPIO_Init(GPIOB, &InitPort);
-	
-		GPIO_StructInit(&InitPort);
+	GPIO_StructInit(&InitPort);
   	InitPort.GPIO_Mode = GPIO_Mode_IPU;
   	InitPort.GPIO_Pin = GPIO_Pin_10;
   	InitPort.GPIO_Speed = GPIO_Speed_2MHz;
@@ -26,7 +25,7 @@ void initLED1(){
   	TIM_TimeBaseStructInit(&timer);
   	timer.TIM_Prescaler = prescaler;
   	timer.TIM_Period = timer_period;
-		TIM_TimeBaseInit(TIM4, &timer);
+	TIM_TimeBaseInit(TIM4, &timer);
  
   	TIM_OCStructInit(&timerPWM);
   	timerPWM.TIM_OCMode = TIM_OCMode_PWM1;
@@ -39,17 +38,17 @@ void initLED1(){
 
 void initLED2(){
 		
-		GPIO_InitTypeDef InitPort;
+	GPIO_InitTypeDef InitPort;
   	TIM_TimeBaseInitTypeDef timer;
   	TIM_OCInitTypeDef timerPWM;
 	
-		GPIO_StructInit(&InitPort);
+	GPIO_StructInit(&InitPort);
   	InitPort.GPIO_Mode = GPIO_Mode_AF_PP;
   	InitPort.GPIO_Pin = GPIO_Pin_8;
   	InitPort.GPIO_Speed = GPIO_Speed_2MHz;
   	GPIO_Init(GPIOB, &InitPort);
 	
-		GPIO_StructInit(&InitPort);
+	GPIO_StructInit(&InitPort);
   	InitPort.GPIO_Mode = GPIO_Mode_IPU;
   	InitPort.GPIO_Pin = GPIO_Pin_10;
   	InitPort.GPIO_Speed = GPIO_Speed_2MHz;
@@ -61,7 +60,7 @@ void initLED2(){
    
   	TIM_TimeBaseInit(TIM4, &timer);
  
-		TIM_OCStructInit(&timerPWM);
+	TIM_OCStructInit(&timerPWM);
   	timerPWM.TIM_OCMode = TIM_OCMode_PWM1;
   	timerPWM.TIM_OutputState = TIM_OutputState_Enable;
   	timerPWM.TIM_Pulse = 50;
@@ -72,7 +71,7 @@ void initLED2(){
 }
 int main(void)
 {
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
   	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 	
 		int TIM_Pulse = 0;
@@ -80,7 +79,7 @@ int main(void)
 		
 	
   	initLED1();
-		//initLED2();
+	//initLED2();
 	
   for(;;)
   {
@@ -95,16 +94,6 @@ int main(void)
 				TIM_Pulse--;
      		TIM4->CCR3 = TIM_Pulse;
 		}
-		for(i=0;i<0x10000;i++);
-			
-			//for(i=0;i<timer_period;i++){
-				//	
-					//TIM4->CCR3 = TIM_Pulse;
-			//}
-			
-			//for(i=timer_period;i>0;i--){
-			
-					
-					
+		for(i=0;i<0x10000;i++);				
   }
 }
